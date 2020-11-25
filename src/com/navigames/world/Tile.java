5c099e58645fc326411214cd986271f41c5e0d54
Rafael Juliano Ferreira <67478757+RafaelJuliano@users.noreply.github.com>
2020-11-25 06:28:43 -0300

    Refatoração World

--- src/com/navigames/world/Tile.java
+++ src/com/navigames/world/Tile.java
@@ -8,8 +8,8 @@ import com.navigames.main.Game;
 
 public class Tile {
 	
-	public static BufferedImage TILE_FLOOR = Game.spritesheet.getSprite(0, 0, 16, 16);
-	public static BufferedImage TILE_WALL = Game.spritesheet.getSprite(16, 0, 16, 16);
+	public static final BufferedImage TILE_FLOOR = Game.spritesheet.getSprite(0, 0, 16, 16);
+	public static final BufferedImage TILE_WALL = Game.spritesheet.getSprite(16, 0, 16, 16);
 
 	private BufferedImage sprite;
 	private int x, y;
--- src/com/navigames/world/World.java
+++ src/com/navigames/world/World.java
@@ -11,40 +11,74 @@ import com.navigames.main.GameScreen;
 public class World {
 
 	private Tile[] tiles;
+	private int[] pixels;
 	public static int WIDTH, HEIGHT;
+	private final int FLOOR = 0xFF000000;
+	private final int WALL = 0xFFFFFFFF;
+	private final int PLAYER = 0xFF00E1FF;
+	private final int ENEMY = 0xFFFF0000;
+	private final int WEAPON = 0xFFFF6A00;
+	private final int LIFE_PACK = 0xFFFFD800;
+	private final int BULLET = 0xFF808080;
+	private int index;
+
+	private BufferedImage map;
 
 	public World(String path) {
 		try {
-			BufferedImage map = ImageIO.read(getClass().getResource(path));
-			WIDTH = map.getWidth();
-			HEIGHT = map.getHeight();
-			int[] pixels = new int[WIDTH * HEIGHT];
-			tiles = new Tile[WIDTH * HEIGHT];
-			map.getRGB(0, 0, WIDTH, HEIGHT, pixels, 0, WIDTH);
-			for (int xx = 0; xx < WIDTH; xx++) {
-				for (int yy = 0; yy < HEIGHT; yy++) {
-					int pixelAtual = pixels[xx + (yy * WIDTH)];
-					tiles[xx + (yy * WIDTH)] = new FloorTile(xx * 16, yy * 16, Tile.TILE_FLOOR);
-					if (pixelAtual == 0xFF000000) {
-						tiles[xx + (yy * WIDTH)] = new FloorTile(xx * 16, yy * 16, Tile.TILE_FLOOR);
-					} else if (pixelAtual == 0xFFFFFFFF) {
-						tiles[xx + (yy * WIDTH)] = new WallTile(xx * 16, yy * 16, Tile.TILE_WALL);
-					} else if (pixelAtual == 0xFF00E1FF) {
-						Game.player.setX(xx * 16);
-						Game.player.setY(yy * 16);
-					} else if (pixelAtual == 0xFFFF0000) {
-						Game.entities.add(new Enemy(xx * 16, yy * 16, 16, 16, Entity.ENEMY_EN));
-					} else if (pixelAtual == 0xFFFF6A00) {
-						Game.entities.add(new Wepon(xx * 16, yy * 16, 16, 16, Entity.WEAPON_EN));
-					} else if (pixelAtual == 0xFFFFD800) {
-						Game.entities.add(new LifePack(xx * 16, yy * 16, 16, 16, Entity.LIFEPACK_EN));
-					} else if (pixelAtual == 0xFF808080) {
-						Game.entities.add(new Bullet(xx * 16, yy * 16, 16, 16, Entity.BULLET_EN));
-					}
-				}
-			}
+			map = ImageIO.read(getClass().getResource(path));
+
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+
+		WIDTH = map.getWidth();
+		HEIGHT = map.getHeight();
+		pixels = new int[WIDTH * HEIGHT];
+		tiles = new Tile[WIDTH * HEIGHT];
+		map.getRGB(0, 0, WIDTH, HEIGHT, pixels, 0, WIDTH);
+		drawMap();		
+	}
+	
+	private void drawMap() {
+		for (int yy = 0; yy < HEIGHT; yy++) {
+			for (int xx = 0; xx < WIDTH; xx++) {
+
+				index = xx + (yy * WIDTH);
+				int posX = xx * 16;
+				int posY = yy * 16;
+
+				int pixelAtual = pixels[index];
+				tiles[index] = new FloorTile(posX, posY, Tile.TILE_FLOOR);
+
+				switch (pixelAtual) {
+				case FLOOR:
+					tiles[index] = new FloorTile(posX, posY, Tile.TILE_FLOOR);
+					break;
+				case WALL:
+					tiles[index] = new WallTile(posX, posY, Tile.TILE_WALL);
+					break;
+				case PLAYER:
+					Game.player.setX(posX);
+					Game.player.setY(posY);
+					break;
+				case ENEMY:
+					Game.entities.add(new Enemy(posX, posY, 16, 16, Entity.ENEMY_EN));
+					break;
+				case WEAPON:
+					Game.entities.add(new Wepon(posX, posY, 16, 16, Entity.WEAPON_EN));
+					break;
+				case LIFE_PACK:
+					Game.entities.add(new LifePack(posX, posY, 16, 16, Entity.LIFEPACK_EN));
+					break;
+				case BULLET:
+					Game.entities.add(new Bullet(posX, posY, 16, 16, Entity.BULLET_EN));
+					break;
+				default:
+					tiles[index] = new FloorTile(posX, posY, Tile.TILE_FLOOR);
+					break;
+				}
+			}
 		}
 	}
 
@@ -55,9 +89,10 @@ public class World {
 		int yfinal = ystart + GameScreen.HEIGHT / 16;
 		for (int xx = xstart; xx <= xfinal; xx++) {
 			for (int yy = ystart; yy <= yfinal; yy++) {
+				index = xx + (yy * WIDTH);
 				if (xx < 0 || yy < 0 || xx >= WIDTH || yy >= HEIGHT)
 					continue;
-				Tile tile = tiles[xx + (yy * WIDTH)];
+				Tile tile = tiles[index];
 				tile.render(g);
 
 			}
